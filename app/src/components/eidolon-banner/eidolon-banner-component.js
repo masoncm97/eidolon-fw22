db64d43316cb450f0b1b52ed76b8152e4e1e2443
masoncm97 <masoncmathew@gmail.com>
2022-11-07 13:53:49 -0500

    More mobile CSS tweaks

--- app/src/components/eidolon-banner/eidolon-banner-component.js
+++ app/src/components/eidolon-banner/eidolon-banner-component.js
@@ -23,9 +23,11 @@ export const EidolonBanner = styled.div`
 export const Heading = styled.h1`
     color: white;
     font-family: Helvetica;
-    font-size: 800%;
+    font-size: 600%;
     text-align: center;
-    max-height: 80vh;
+    @media screen and (min-width: 1024px) { 
+       font-size: 800%;
+    }
 `;
 
 export const Copyright = styled.p`
@@ -34,6 +36,16 @@ export const Copyright = styled.p`
     margin-bottom: 15px;
     position: fixed;
     bottom: 0;
+    display: none;
+
+    @media screen and (min-width: 1024px) { 
+        color: white;
+        font-size: .8em;
+        margin-bottom: 15px;
+        position: fixed;
+        bottom: 0; 
+        display: block;
+    }
 `;
 
 export default EidolonBannerComponent;
